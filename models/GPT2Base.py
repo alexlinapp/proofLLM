f3dcd56e57ecd8f972ea0da3f76c183202125589
Alex <alexlinapp@gmail.com>
2025-08-16 14:56:35 -0500

    updated base model

--- models/GPT2Base.py
+++ models/GPT2Base.py
@@ -86,23 +86,23 @@ class TransformerBlock(nn.Module):
     return x
 
 class GPTModel(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-        self.tok_emb = torch.nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
-        self.pos_emb = torch.nn.Embedding(cfg["context_length"], cfg["emb_dim"])
-        self.drop_emb = nn.Dropout(cfg["drop_rate"])
-        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
-        self.final_norm = LayerNorm(cfg["emb_dim"])
-        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+  def __init__(self, cfg):
+      super().__init__()
+      self.tok_emb = torch.nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+      self.pos_emb = torch.nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+      self.drop_emb = nn.Dropout(cfg["drop_rate"])
+      self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+      self.final_norm = LayerNorm(cfg["emb_dim"])
+      self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
 
 
-    def forward(self, x):
-        bs, seq_len = x.shape
-        tok_embeds = self.tok_emb(x)
-        pos_embeds = self.pos_emb(torch.arange(seq_len, device=x.device))
-        x = tok_embeds + pos_embeds
-        x = self.drop_emb(x)
-        x = self.trf_blocks(x)
-        x = self.final_norm(x)
-        logits = self.out_head(x)
-        return logits
+  def forward(self, x):
+      bs, seq_len = x.shape
+      tok_embeds = self.tok_emb(x)
+      pos_embeds = self.pos_emb(torch.arange(seq_len, device=x.device))
+      x = tok_embeds + pos_embeds
+      x = self.drop_emb(x)
+      x = self.trf_blocks(x)
+      x = self.final_norm(x)
+      logits = self.out_head(x)
+      return logits
